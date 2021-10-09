3c70cd39f1102bd07e0d877a0e72fa45f89df9e1
Rodrigo Bossini <professorbossini@gmail.com>
2021-10-09 11:57:55 -0300

    Encerrada a secao 2.20 da apostila 08. NO ultimo bloco de codigo, fizemos o if/else para evitar tempo de espera inicial

--- src/components/Busca.js
+++ src/components/Busca.js
@@ -1,19 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import { InputText } from 'primereact/inputtext'
+import { Button } from 'primereact/button'
+import axios from 'axios'
+import striptags from 'striptags'
+//https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=java
 const Busca = () => {
     
-    const [termoDeBusca, setTermoDeBusca] = useState('')
+    const [termoDeBusca, setTermoDeBusca] = useState('React')
+    const [resultados, setResultados] = useState([])
+    console.log(resultados)
+    
     useEffect(() => {
-        console.log ("Executando todas as vezes...")
-    })
-
-    useEffect(() => {
-        console.log ("Executando uma única vez...")
-    }, [])
-
-    useEffect(() => {
-        console.log ("Executando somente se o termo tiver sido atualizado")
+        const fazBusca = async () => {
+            const { data } = await axios.get (
+                'https://en.wikipedia.org/w/api.php',
+                {
+                    params: {
+                       action: 'query',
+                       list: 'search',
+                       format: 'json',
+                       origin: '*',
+                       srsearch: termoDeBusca 
+                    }
+                }
+            )
+            setResultados(data.query.search)
+        }
+        if (termoDeBusca && !resultados.length){
+            fazBusca()
+        }
+        else{
+            const timeoutID = setTimeout(() => {
+                if (termoDeBusca)
+                    fazBusca()
+            }, 1000)
+            return () => {
+                clearInterval(timeoutID)
+            }
+        }
     }, [termoDeBusca])
+    
+    
     return (
         <div>
             <span className="p-input-icon-left">
@@ -22,6 +49,37 @@ const Busca = () => {
                     onChange={(e) => setTermoDeBusca(e.target.value)}
                 />
             </span>
+            {
+                resultados.map((resultado) => (
+                    <div 
+                        key={resultado.pageid}
+                        className="my-2 border border-1 border-400"
+                    >
+                        <div
+                            className="border border-bottom border-1 border-400 p-2 text-center font-bold">
+                            {resultado.title}
+                            <span>
+                                <Button 
+                                    icon="pi pi-send"
+                                    className="ml-2 p-button-rounded p-button-secondary"
+                                    onClick={() => window.open(
+                                        `https://en.wikipedia.org?curid=${resultado.pageid}`,
+                                        '_blank'
+                                    )}
+                                />
+                            </span>
+                        </div>
+                        <div className="p-2">
+                            <span
+                                // XSS
+                                dangerouslySetInnerHTML={{__html: resultado.snippet}}>
+
+                            </span>
+                        </div>
+
+                    </div>
+                ))
+            }
         </div>
     )
 }
